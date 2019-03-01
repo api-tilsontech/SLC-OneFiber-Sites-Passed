fdca8e51331d7093c58993e0cd9760b0f7d55c43
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2019-03-01 10:45:34 -0700

    asdf

--- scripts/base.js
+++ scripts/base.js
@@ -11,7 +11,7 @@ function verifyUser() {
     $(".navbar-collapse").css("opacity", "0");
     window.confirm
     if (window.confirm('Click OK to go to Login Page')) {
-      window.location.href='https://api-tilsontech.github.io/SLC-OneFiber_DataMap_NEW/index.html';
+      window.location.href='main.html';
     };
   };
 };

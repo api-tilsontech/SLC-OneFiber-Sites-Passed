480a63510eafec5f3a76fc75fa53f670cb648d1d
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2019-03-02 18:36:31 -0700

    asdf

--- scripts/gisSites.js
+++ scripts/gisSites.js
@@ -544,7 +544,7 @@ function gisSitesBuildTable() {
     "searching": true, // Toggle search all columns field
     "stateSave": false, // If true, table will restore to user filtered state when page is reopened     
     "scrollCollapse": true, // If true, the table will be collapsed if the height of the records is < the scrollY option; prevents footer from floating
-    "columns": gisSitesTableColumns,
+    "columns": [gisSitesTableColumns],
     "language": {
       "emptyTable": "Loading..."
     }

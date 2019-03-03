021011977ed05c774e93579eafaca5d1277a0f07
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2019-03-02 22:06:58 -0700

    asdf

--- scripts/gisSegments.js
+++ scripts/gisSegments.js
@@ -377,6 +377,10 @@ function gisSegmentsInfo(id) {
     }
     $.each(gisSegmentsProperties, function(index, property) {
       if (key == property.value) {
+        if (value && property.filter.value == "date") {
+          date = new Date(value);
+          value = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+        }
         if (property.info !== false) {
           content += "<tr><th>" + property.label + "</th><td>" + value + "</td></tr>";
         }

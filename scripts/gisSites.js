fd138fdec30060ae7beaaf31b2c6f7f68af762bc
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2019-03-03 14:52:31 -0700

    asdf

--- scripts/gisSites.js
+++ scripts/gisSites.js
@@ -502,7 +502,7 @@ function gisSitesBuildTable() {
     "ordering": true, // Toggle user ordering of table columns
     "paging": false, // Toggle table paging
     "processing": true, // Toggle "processing" indicator useful when loading large table/filter
-    "scrollX": false, // Left/right scrolling option, in pixels or false to disable
+    "scrollX": true, // Left/right scrolling option, in pixels or false to disable
     "scrollY": "550px", // Table height in pixels before up/down scrolling, or false to disable scrolling
     "searching": true, // Toggle search all columns field
     "stateSave": true, // If true, table will restore to user filtered state when page is reopened     

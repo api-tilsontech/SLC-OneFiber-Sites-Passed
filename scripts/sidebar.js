65d90f5ec61e64952dd650b98af0c8bee10c96f4
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2019-05-18 01:10:52 -0600

    asdf

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -4,12 +4,44 @@ $("#gisSitesClose-sidebarBTN").click(function(){
   gisSitesSidebar.hide();
 });
 
-
-var gisSitesSidebar = L.control.sidebar("gisSitesSidebar", {
-    closeButton: false,
-    position: "right",
-    autoPan: false
-}).addTo(map);
+var sidebar = L.control.sidebar({ container: 'gisSitesSidebar' })
+    .addTo(map)
+    .open('home');
+// add panels dynamically to the sidebar
+sidebar
+    .addPanel({
+        id:   'js-api',
+        tab:  '<i class="fa fa-gear"></i>',
+        title: 'JS API',
+        pane: '<p>The Javascript API allows to dynamically create or modify the panel state.<p/><p><button onclick="sidebar.enablePanel(\'mail\')">enable mails panel</button><button onclick="sidebar.disablePanel(\'mail\')">disable mails panel</button></p><p><button onclick="addUser()">add user</button></b>',
+    })
+    // add a tab with a click callback, initially disabled
+    .addPanel({
+        id:   'mail',
+        tab:  '<i class="fa fa-envelope"></i>',
+        title: 'Messages',
+        button: function() { alert('opened via JS callback') },
+        disabled: true,
+    })
+// be notified when a panel is opened
+sidebar.on('content', function (ev) {
+    switch (ev.id) {
+        case 'autopan':
+        sidebar.options.autopan = true;
+        break;
+        default:
+        sidebar.options.autopan = false;
+    }
+});
+var userid = 0
+function addUser() {
+    sidebar.addPanel({
+        id:   'user' + userid++,
+        tab:  '<i class="fa fa-user"></i>',
+        title: 'User Profile ' + userid,
+        pane: '<p>user ipsum dolor sit amet</p>',
+    });
+}
 
 
 $("#gisSitesTable-btn").click(function(){
